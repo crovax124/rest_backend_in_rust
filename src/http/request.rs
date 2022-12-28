69cf5a2388b9bcffd948aa1422076f87d30de851
crovax124 <eric.isensee11@gmail.com>
2022-12-28 21:30:09 +0900

    implemented Type conversion

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
@@ -6,3 +7,38 @@ pub struct Request {
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+/*
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let string = String::from("asd");
+        string.encrypt();
+        value.encrypt();
+        todo!()
+    }
+}
+
+//traits are abstract classes and the for keyword is to extend classes.
+trait Encrypt {
+    fn encrypt(&self) -> Self;
+}
+impl Encrypt for String {
+    fn encrypt(&self) -> Self {
+        todo!()
+    }
+}
+impl  Encrypt for &[u8] {
+    fn encrypt(&self) -> Self {
+        todo!()
+    }
+}*/
+
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
 
 pub struct Server {
     address: String,
@@ -24,7 +26,13 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(result) => {
-                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+
+                          match Request::try_from(&buffer[..]) {
+                              Ok(result) => {}
+                              Err(e) => println!("Failed to parse a request: {}", e),
+                          }
                         }
                         Err(e) => println!("Failed to read the Websocket: {}", e),
                     }

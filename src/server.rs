38ef8bc615c8729afd4e26b0bbb9d9d7ddf651cc
crovax124 <eric.isensee11@gmail.com>
2022-12-28 20:58:09 +0900

    implemented a listener for get Requests

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+use std::io::Read;
+
 pub struct Server {
     address: String,
 }
+
 
 impl Server {
     pub fn new(address: String) -> Self {
@@ -9,6 +13,26 @@ impl Server {
         }
     }
     pub fn run(self) {
-        println!("Listening on: {}", self.address)
+        println!("Listening on: {}", self.address);
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        'outer: loop {
+            //Match is a just a sugar syntax for switch statements
+            match listener.accept() {
+                Ok((mut stream, address)) => {
+                    println!("Connection established on {}: ", address);
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(result) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to read the Websocket: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+
+                // _ => {} default case
+            }
+        }
     }
 }

a471a63a40050cdc26c46b86f118da6f218f83d3
crovax124 <eric.isensee11@gmail.com>
2023-01-03 16:50:17 +0900

    implemented Request handling and return trait

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+use crate::http::ParseError;
+
 pub enum Method {
     GET,
     POST,
@@ -10,3 +13,22 @@ pub enum Method {
     PATCH,
 }
 
+impl FromStr for Method {
+    type Err = MethodError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "DELETE" => Ok(Self::DELETE),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            _ => Err(MethodError)
+        }
+    }
+}
+
+pub struct MethodError;
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,10 +1,11 @@
-use super::method::Method;
+use super::method::{MethodError, Method};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
 use std::fmt::Result as FmtResult;
 use std::str;
 use std::str::Utf8Error;
+
 
 pub struct Request {
     path: String,
@@ -20,17 +21,42 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
+        /*
+        match get_next_word(request) {
+            None => return Err(ParseError::InvalidRequest),
+            Some((method,request)) => {}
+        }
+        */
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+        let mut query_string = None;
+        if let Some(i) = path.find('?'){
+            query_string = Some(path[i+1..].to_string());
+            path = &path[..i];
+        }
+   Ok(Self{
+       path: path.to_string(),
+       query_string: None,
+       method,
+   })
     }
 }
 
-fn get_next_word(request: &str) -> Option<(&str, &str)>{
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
-            return Some((&request[..i], &request[i+1..]));
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
         }
     }
     None
 }
+
 pub enum ParseError {
     InvalidRequest,
     InvalidEncoding,
@@ -49,11 +75,19 @@ impl Display for ParseError {
         write!(f, "{}", self.message())
     }
 }
+
 impl From<Utf8Error> for ParseError {
-    fn from(_: Utf8Error) -> Self{
+    fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
     }
 }
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
+}
+
 impl ParseError {
     fn message(&self) -> &str {
         match self {

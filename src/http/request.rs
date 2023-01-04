435c2614c963e11ce20601dc74629f5b0383b6d7
crovax124 <eric.isensee11@gmail.com>
2023-01-04 17:03:29 +0900

    lifetime practise

--- src/http/request.rs
+++ src/http/request.rs
@@ -7,18 +7,18 @@ use std::str;
 use std::str::Utf8Error;
 
 
-pub struct Request {
-    path: String,
+pub struct Request<'buf> {
+    path: &'buf str,
     //option is whether none or some value of the Type specified
-    query_string: Option<String>,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     //GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf[u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         /*
@@ -37,11 +37,11 @@ impl TryFrom<&[u8]> for Request {
         let method: Method = method.parse()?;
         let mut query_string = None;
         if let Some(i) = path.find('?'){
-            query_string = Some(path[i+1..].to_string());
+            query_string = Some(&path[i+1..]);
             path = &path[..i];
         }
    Ok(Self{
-       path: path.to_string(),
+       path,
        query_string: None,
        method,
    })
